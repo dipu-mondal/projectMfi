fc3b4ce1ed5b14b87129d1a8437aa02abf558197
dipu-mondal <dipu.ais.du@gmail.com>
2024-02-26 23:01:10 +0600

    Modification on the test file

--- testjs.js
+++ testjs.js
@@ -14,14 +14,40 @@
 //   await getData(4);
 // }
 // allDataAPI();
-const URL = "https://jsonplaceholder.typicode.com/users";
-async function getInformation() {
-  let response = await fetch(URL);
-  let data = await response.json();
-  data.forEach((element) => {
-    console.log(
-      `Name: ${element.name} \nEmail: ${element.email} \nStreet: ${element.address.street}\nCity: ${element.address.city}`
+// const URL = "https://jsonplaceholder.typicode.com/users";
+// async function getInformation() {
+//   let response = await fetch(URL);
+//   let data = await response.json();
+//   data.forEach((element) => {
+//     console.log(
+//       `Name: ${element.name} \nEmail: ${element.email} \nStreet: ${element.address.street}\nCity: ${element.address.city}`
+//     );
+//   });
+// }
+// getInformation();
+
+function getLocation() {
+  return new Promise((resolve, reject) => {
+    navigator.geolocation.getCurrentPosition(
+      (position) => {
+        resolve(position);
+        reject("Did not get value");
+      },
+      () => {
+        alert("No location Found");
+      }
     );
   });
 }
-getInformation();
+
+// async function gettingPos() {
+//   let val = await fetch(`https://restcountries.com/v3.1/name/bangladesh`);
+//   if (val.ok === false) throw new Error("SOMETHING WRONG GONE!!");
+//   let data = await val.json();
+//   console.log(data);
+//   return "Testing Return";
+// }
+// (async function () {
+//   let val = await gettingPos();
+//   console.log(val);
+// })();
